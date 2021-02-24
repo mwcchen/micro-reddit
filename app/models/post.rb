41f79976aec51c843974be1a8028d3e6ad320c37
mwcchen <33730138+mwcchen@users.noreply.github.com>
2021-02-24 17:17:29 -0500

    Added Comment associations and updated Post and User associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   validates :body, presence: true
   validates :user_id, presence: true
   belongs_to :user
+  has_many :comments
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..16 }
   has_many :posts
+  has_many :comments
+
 end
