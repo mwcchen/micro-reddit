1b3db668cdc8d19e164393b24ea3a4459f25bdf1
mwcchen <33730138+mwcchen@users.noreply.github.com>
2021-02-24 17:03:49 -0500

    Added User and Post associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true
   validates :user_id, presence: true
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { in: 4..12 }, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..16 }
+  has_many :posts
 end
